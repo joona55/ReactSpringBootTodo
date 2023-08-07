d4d53c9b401f565d86e495abea34c2941c250824
ParkJoonha <parkaso1@gmail.com>
2023-08-07 21:27:02 +0900

    react-router-dom 연습

--- src/components/Todo/TodoApp.jsx
+++ src/components/Todo/TodoApp.jsx
@@ -1,11 +1,18 @@
 import './TodoApp.css'
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
 import { useState } from 'react'
+
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            Todo Management Application
-            <LoginComponent />
-            {/* <WelcomeComponent /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginComponent />}></Route>
+                    <Route path='/login' element={<LoginComponent />}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
+                    <Route path='*' element={<ErrorComponent />}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
@@ -14,8 +21,10 @@ export default function TodoApp() {
 function LoginComponent() {
 
     const [username, setUsername] = useState('input name')
-
     const [password, setPassword] = useState('')
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+    const navigate = useNavigate();
 
     function handleUsernameChange(event) {
         setUsername(event.target.value)
@@ -25,8 +34,23 @@ function LoginComponent() {
         setPassword(event.target.value)
     }
 
+    function handleSubmit() {
+        if(username === 'joona55' && password === 'dummy') {
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+            navigate(`/welcome/${username}`)
+        }
+        else {
+            setShowErrorMessage(true)
+            setShowSuccessMessage(false)
+        }
+    }
+
     return (
         <div className="Login">
+            <h1>Login Page</h1>
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage"> Authenticated Failed. Please check your credentials.</div>}
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
@@ -37,7 +61,7 @@ function LoginComponent() {
                     <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
@@ -45,9 +69,39 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+
+    const {username} = useParams()
+
     return (
-        <div className="Welcome">
-            Welcome Component
+        <div className="WelcomeComponent">
+            <h1>Welcome Todo Board. {username}</h1>
+            <div>
+                Welcome Component
+            </div>
         </div>
     )
 }
+
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>Apologies for the 404.</h1>
+            <div>
+                Contact: 000-0000-0000.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+    return (
+        <div className="ListTodosComponent">
+            <h1>To do List</h1>
+            <div>
+                Todo Details.
+            </div>
+        </div>
+    )
+}
+
+
