3a3de90d7a7c64a6cde1490f976c5faf40be1ab5
ParkJoonha <parkaso1@gmail.com>
2023-08-08 00:37:28 +0900

    react component 리펙토링

--- src/components/Todo/TodoApp.jsx
+++ src/components/Todo/TodoApp.jsx
@@ -1,107 +1,28 @@
-import './TodoApp.css'
-import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import './TodoApp.css'
+import HeaderComponent from './HeaderComponent'
+import FooterComponent from './FooterComponent'
+import LoginComponent from './LoginComponent'
+import WelcomeComponent from './WelcomeComponent'
+import LogoutComponent from './LogoutComponent'
+import ListTodosComponent from './ListTodosComponent'
+import ErrorComponent from './ErrorComponent'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
-                    <Route path='/' element={<LoginComponent />}></Route>
-                    <Route path='/login' element={<LoginComponent />}></Route>
-                    <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
-                    <Route path='*' element={<ErrorComponent />}></Route>
+                    <Route path='/' element={<LoginComponent />} />
+                    <Route path='/login' element={<LoginComponent />} />
+                    <Route path='/logout' element={<LogoutComponent />} />
+                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
+                    <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='*' element={<ErrorComponent />} />
                 </Routes>
             </BrowserRouter>
         </div>
     )
 }
-
-// 사용자 이름, 비밀번호
-function LoginComponent() {
-
-    const [username, setUsername] = useState('input name')
-    const [password, setPassword] = useState('')
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-    const [showErrorMessage, setShowErrorMessage] = useState(false)
-    const navigate = useNavigate();
-
-    function handleUsernameChange(event) {
-        setUsername(event.target.value)
-    }
-
-    function handlePasswordChange(event) {
-        setPassword(event.target.value)
-    }
-
-    function handleSubmit() {
-        if(username === 'joona55' && password === 'dummy') {
-            setShowSuccessMessage(true)
-            setShowErrorMessage(false)
-            navigate(`/welcome/${username}`)
-        }
-        else {
-            setShowErrorMessage(true)
-            setShowSuccessMessage(false)
-        }
-    }
-
-    return (
-        <div className="Login">
-            <h1>Login Page</h1>
-            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
-            {showErrorMessage && <div className="errorMessage"> Authenticated Failed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name</label>
-                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-                </div>
-                <div>
-                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function WelcomeComponent() {
-
-    const {username} = useParams()
-
-    return (
-        <div className="WelcomeComponent">
-            <h1>Welcome Todo Board. {username}</h1>
-            <div>
-                Welcome Component
-            </div>
-        </div>
-    )
-}
-
-function ErrorComponent() {
-    return (
-        <div className="ErrorComponent">
-            <h1>Apologies for the 404.</h1>
-            <div>
-                Contact: 000-0000-0000.
-            </div>
-        </div>
-    )
-}
-
-function ListTodosComponent() {
-    return (
-        <div className="ListTodosComponent">
-            <h1>To do List</h1>
-            <div>
-                Todo Details.
-            </div>
-        </div>
-    )
-}
-
-
