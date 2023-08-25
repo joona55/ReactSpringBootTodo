a5550a31b0e24b828f15dac0a362aa1a8f0be300
ParkJoonha <parkaso1@gmail.com>
2023-08-26 04:24:14 +0900

    axios 호출 추가

--- src/components/Todo/WelcomeComponent.jsx
+++ src/components/Todo/WelcomeComponent.jsx
@@ -1,14 +1,38 @@
 import { Link, useParams } from 'react-router-dom'
+import axios, { Axios } from 'axios'
 
 export default function WelcomeComponent() {
 
     const {username} = useParams()
+
+    function callHelloWorldRestApi() {
+        console.log('called')
+        // REST API 호출 프레임워크 : axios 
+        axios.get('http://localhost:8080/hello-world')
+            .then( (response) => successfulResponse(response) )
+            .catch( (error) => errorResponse(error) )
+            .finally( () => console.log('cleanup') )
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
 
     return (
         <div className="WelcomeComponent">
             <h1>Welcome Todo Board. {username}</h1>
             <div>
                 Your todos. <Link to="/todos">Go Todo Page</Link>
+            </div>
+            <div>
+                <button className="btn btn-success" onClick={callHelloWorldRestApi}>
+                    Call Hello Wolrd REST API
+                </button>
             </div>
         </div>
     )
