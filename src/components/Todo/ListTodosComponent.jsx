c2539198030f2a54bedb742dc51ad11d434954fa
ParkJoonha <parkaso1@gmail.com>
2023-10-06 04:38:34 +0900

    Todo Delete 기능 추가

--- src/components/Todo/ListTodosComponent.jsx
+++ src/components/Todo/ListTodosComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { retrieveAllTodosUsername } from "./api/TodoApiService";
+import { deleteTodoApi, retrieveAllTodosUsername, retrieveAllTodosUsernameApi } from "./api/TodoApiService";
 
 export default function ListTodosComponent() {
 
@@ -9,28 +9,41 @@ export default function ListTodosComponent() {
 
     const [todos, setTodos] = useState([])
 
+    const [message, setMessage] = useState(null)
+
     useEffect(() => refreshTodos, [])
 
     function refreshTodos() {
-        retrieveAllTodosUsername('joona55')
+        retrieveAllTodosUsernameApi('joona55')
         .then(response => {
             console.log(response.data)
             setTodos(response.data)
         })
         .catch(error => console.log(error))
     }
+
+    function deleteTodos(id) {
+        console.log('clicked ' + id)
+        deleteTodoApi('joona55', id)
+        .then(
+            setMessage(`Delete of todo with ${id} success`),
+            refreshTodos()
+        )
+        .catch(error => console.log(error))
+    }
     
     return (
         <div className="container">
             <h1>To do List</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                 <table className='table'>
                     <thead>
                         <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
+                            <th>Description</th>
+                            <th>Is Done?</th>
+                            <th>Target Date</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -40,11 +53,12 @@ export default function ListTodosComponent() {
                                 // 리스트를 사용할 때 key 값 설정을 통해 각 요소가
                                 // 유일한 키를 갖게 해야한다.
                                 <tr key={todo.id}>
-                                    <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
                                     {/* <td>{todo.targetDate.toDateString()}</td> */}
                                     <td>{todo.targetDate.toString()}</td>
+                                    <td><button className="btn btn-warning" 
+                                                onClick={() => deleteTodos(todo.id)}>Delete</button></td>
                                 </tr>
                             )
                         )
--- src/components/Todo/api/TodoApiService.js
+++ src/components/Todo/api/TodoApiService.js
@@ -10,5 +10,10 @@ const apiClient = axios.create(
     }
 )
 
-export const retrieveAllTodosUsername
+export const retrieveAllTodosUsernameApi
     = (username) => apiClient.get(`/users/${username}/todos`)
+    //http://localhost:8080/users/joona55/todos
+
+export const deleteTodoApi
+    = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
+    //http://localhost:8080/users/joona55/todos/1
